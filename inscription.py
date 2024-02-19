bb9af75cb406746b711f742206db2558742ec3e4
elias-ziane <elias-ziane@github.com>
2024-02-19 12:46:53 +0100

    changement design, ajout logo

--- inscription.py
+++ inscription.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import Label, Entry, Button, messagebox
 from utilisateurs import Utilisateurs
+from PIL import Image, ImageTk
 
 class PageInscription:
     def __init__(self, root):
@@ -17,6 +18,7 @@ class PageInscription:
         x_position = (screen_width - 800) // 2
         y_position = (screen_height - 600) // 2
         self.root.geometry(f"800x600+{x_position}+{y_position}")
+
         # Configurer les colonnes pour qu'elles s'étirent horizontalement
         self.root.columnconfigure(0, weight=1)
         self.root.columnconfigure(1, weight=1)
@@ -25,33 +27,54 @@ class PageInscription:
         for i in range(7):  # Augmenté à 7 pour ajouter une ligne pour le titre
             self.root.rowconfigure(i, weight=1)
 
+        # Changer la couleur de fond de la fenêtre
+        self.root.configure(bg="#313339")
+
         # Ajouter le titre au-dessus des labels
-        Label(self.root, text="INSCRIPTION", font=("Helvetica", 20)).grid(row=0, column=0, columnspan=2, pady=(10, 0))
+        Label(self.root, text="INSCRIPTION", font=("Helvetica", 20), bg="#313339", fg="white").grid(row=0, column=0, columnspan=2, pady=(10, 0))
+
+        # Ajouter le logo
+        self.ajouter_logo()
 
         # Création des labels et entry widgets pour chaque champ du produit
-        Label(self.root, text="Nom :", font=("Helvetica", 14)).grid(row=1, column=0, sticky="e", pady=(7, 0))
-        self.nom_entry = Entry(self.root)
-        self.nom_entry.grid(row=1, column=1, sticky="w", pady=(7, 0), ipady=3)
+        label_styles = {"font": ("Helvetica", 14), "bg": "#313339", "fg": "#8A8E92"}
+        entry_styles = {"bg": "#424549", "fg": "white"}
 
-        Label(self.root, text="Prenom :", font=("Helvetica", 14)).grid(row=2, column=0, sticky="e", pady=(7, 0))
-        self.prenom_entry = Entry(self.root)
-        self.prenom_entry.grid(row=2, column=1, sticky="w", pady=(7, 0), ipady=3)
+        Label(self.root, text="Nom :", **label_styles).grid(row=2, column=0, sticky="e", pady=(7, 0))
+        self.nom_entry = Entry(self.root, **entry_styles)
+        self.nom_entry.grid(row=2, column=1, sticky="w", pady=(7, 0), ipady=3)
 
-        Label(self.root, text="Email :", font=("Helvetica", 14)).grid(row=3, column=0, sticky="e", pady=(7, 0))
-        self.email_entry = Entry(self.root)
-        self.email_entry.grid(row=3, column=1, sticky="w", pady=(7, 0), ipady=3)
+        Label(self.root, text="Prenom :", **label_styles).grid(row=3, column=0, sticky="e", pady=(7, 0))
+        self.prenom_entry = Entry(self.root, **entry_styles)
+        self.prenom_entry.grid(row=3, column=1, sticky="w", pady=(7, 0), ipady=3)
 
-        Label(self.root, text="Mot de passe :", font=("Helvetica", 14)).grid(row=4, column=0, sticky="e", pady=(7, 0))
-        self.mot_de_passe_entry = Entry(self.root, show="*")
-        self.mot_de_passe_entry.grid(row=4, column=1, sticky="w", pady=(7, 0), ipady=3)
+        Label(self.root, text="Email :", **label_styles).grid(row=4, column=0, sticky="e", pady=(7, 0))
+        self.email_entry = Entry(self.root, **entry_styles)
+        self.email_entry.grid(row=4, column=1, sticky="w", pady=(7, 0), ipady=3)
 
+        Label(self.root, text="Mot de passe :", **label_styles).grid(row=5, column=0, sticky="e", pady=(7, 0))
+        self.mot_de_passe_entry = Entry(self.root, show="*", **entry_styles)
+        self.mot_de_passe_entry.grid(row=5, column=1, sticky="w", pady=(7, 0), ipady=3)
 
-        # Bouton "Cancel"
-        cancel_button = Button(self.root, text="Annuler", command=self.root.destroy)
-        cancel_button.grid(row=6, column=0, pady=10, sticky="nsew")
-        # Bouton "Enregistrer le produit"
-        save_button = Button(self.root, text="Enregistrer", command=self.save_user)
-        save_button.grid(row=6, column=1, pady=10, sticky="nsew")
+        # Changer la couleur de fond des boutons
+        button_styles = {"bg": "#424549", "fg": "white"}
+
+        cancel_button = Button(self.root, text="Annuler", command=self.root.destroy, **button_styles)
+        cancel_button.grid(row=7, column=0, pady=10, sticky="nsew")
+
+        save_button = Button(self.root, text="Enregistrer", command=self.save_user, **button_styles)
+        save_button.grid(row=7, column=1, pady=10, sticky="nsew")
+
+    def ajouter_logo(self):
+        # Charger le logo
+        logo_image = Image.open("images/logo-discord.png")
+        logo_image = logo_image.resize((100, 100), Image.LANCZOS)
+        logo_photo = ImageTk.PhotoImage(logo_image)
+
+        # Créer un label pour afficher le logo
+        logo_label = Label(self.root, image=logo_photo, bg="#313339")
+        logo_label.image = logo_photo  # Gardez une référence pour éviter la suppression par le garbage collector
+        logo_label.grid(row=1, column=0, columnspan=2, pady=(10, 0))
 
     def save_user(self):
         nom = self.nom_entry.get()
@@ -68,8 +91,6 @@ class PageInscription:
             messagebox.showerror("Erreur", "Mot de passe invalide.")
             return
 
-    
-
         # Appel de la méthode pour enregistrer l'utilisateur dans la base de données
         add_result = self.utilisateur.Create_utilisateur(nom, prenom, email, mot_de_passe)
         self.root.destroy()  # Fermer la fenêtre après avoir ajouté l'utilisateur
@@ -78,7 +99,6 @@ class PageInscription:
             messagebox.showinfo("Succès", "Votre inscription est complète.")
         else:
             messagebox.showerror("Erreur", "Une erreur s'est produite lors de l'inscription.")
-
 
 if __name__ == "__main__":
     root = tk.Tk()
